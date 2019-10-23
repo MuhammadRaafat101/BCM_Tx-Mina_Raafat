41b63de43f1d6af6a8eb27a966ee0e002ffacdfb
Mraafat32 <muhammadraafatrashad@gmail.com>
2019-10-23 22:09:16 +0200

    Create stdtypes.h

---
+++ stdtypes.h
@@ -0,0 +1,15 @@
+#ifndef STDTYPES_H
+#define STDTYPES_H
+typedef unsigned char uint8;
+typedef signed char sint8;
+typedef unsigned short int uint16;
+typedef signed short int sint16;
+typedef unsigned int uint32;
+typedef signed int sint32;
+typedef unsigned int uint64;
+typedef signed int sint64;
+typedef float float32;
+typedef double float64;
+typedef long double float128;
+
+#endif
